59950f7eb87bffcee6b3bddc2fad63f0bc68248d
k4u5h4L <kaushal.v.bhat@gmail.com>
2021-03-01 13:32:52 +0530

    Add dynamic query

--- pages/video/index.js
+++ pages/video/index.js
@@ -9,10 +9,19 @@ import Slider from "../../components/Home/Slider/Slider";
 import Footer from "../../components/Home/Footer/Footer";
 
 import { PrismaClient } from "@prisma/client";
+import { useRouter } from "next/router";
 
 const prisma = new PrismaClient();
 
-const Video = ({ movie }) => {
+const Video = ({ movie, status }) => {
+    if (typeof window !== "undefined") {
+        const router = useRouter();
+
+        if (status == false) {
+            router.push("/");
+        }
+    }
+
     return (
         <div>
             {/* <Preloader /> */}
@@ -29,12 +38,25 @@ const Video = ({ movie }) => {
     );
 };
 
-export async function getServerSideProps(context) {
-    const movieId = parseInt(context.params.id);
+export const getServerSideProps = async (context) => {
+    const movieId = parseInt(context.query.id);
+    if (!movieId) {
+        return {
+            props: {
+                movie: {
+                    id: 9,
+                    name: "Error",
+                    desc: "Bell",
+                },
+                status: false,
+            },
+        };
+    }
     const movie = await prisma.movie.findFirst({ where: { id: movieId } });
     return {
         props: {
             movie: movie,
+            status: true,
         },
     };
 
@@ -47,6 +69,6 @@ export async function getServerSideProps(context) {
     //         },
     //     },
     // };
-}
+};
 
 export default Video;
