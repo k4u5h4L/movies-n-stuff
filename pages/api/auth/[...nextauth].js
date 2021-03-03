95bd5e65364d9a6d93b184e6f2d7bd9e8951806f
k4u5h4L <kaushal.v.bhat@gmail.com>
2021-03-03 14:15:30 +0530

    Add auth with sessions and jwt

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import Adapters from "next-auth/adapters";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default (req, res) =>
     NextAuth(req, res, {
@@ -13,5 +17,11 @@ export default (req, res) =>
         secret: process.env.AUTH_SECRET,
         jwt: {
             secret: process.env.JWT_SECRET,
+        },
+        adapter: Adapters.Prisma.Adapter({ prisma }),
+        callbacks: {
+            async redirect(url, baseUrl) {
+                return "/";
+            },
         },
     });
