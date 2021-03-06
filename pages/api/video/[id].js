100627e6ed5aae72199d8c58d50fe87e12f44087
k4u5h4L <kaushal.v.bhat@gmail.com>
2021-03-06 18:39:52 +0530

    Fix streaming api

--- pages/api/video/[id].js
+++ pages/api/video/[id].js
@@ -1,18 +1,21 @@
 import fs from "fs";
 import prisma from "../../../prisma/client";
 
+// is you're using next.js 10.0.7, webpack versions sometimes don't work
 // "webpack": "4.44.1"
 
+/*
+// old code
 export default async (req, res) => {
     const { id } = req.query;
 
     const movie = await prisma.movie.findFirst({ where: { id: parseInt(id) } });
-    // console.log(movie);
 
     try {
         const range = req.headers.range;
         if (!range) {
             res.status(400).send("Requires Range header");
+            return;
         }
 
         // get video stats (about 61MB)
@@ -45,5 +48,38 @@ export default async (req, res) => {
     } catch (err) {
         console.error(err);
         res.status(400).send("Error");
+    }
+};
+*/
+
+export default async (req, res) => {
+    const { id } = req.query;
+
+    const movie = await prisma.movie.findFirst({ where: { id: parseInt(id) } });
+    const path = movie.vidSource;
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+        const chunksize = end - start + 1;
+        const file = fs.createReadStream(path, { start, end });
+        const head = {
+            "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+            "Accept-Ranges": "bytes",
+            "Content-Length": chunksize,
+            "Content-Type": "video/mp4",
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            "Content-Length": fileSize,
+            "Content-Type": "video/mp4",
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(path).pipe(res);
     }
 };
